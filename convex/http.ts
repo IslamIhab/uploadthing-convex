3d6d0dabf8e592e1cd6fce79864508b1264d3914
islamihab <islam@ryukaian.com>
2025-09-03 02:29:11 +0300

    Update uploadthing dependency path and modify route handler in Convex setup

--- convex/http.ts
+++ convex/http.ts
@@ -2,9 +2,8 @@ import { httpRouter } from "convex/server";
 import { createRouteHandler } from "uploadthing/convex-helpers";
 import { internal } from "./_generated/api";
 
-
 const http = httpRouter();
 
-createRouteHandler(http, internal.uploadthing.handler);
+createRouteHandler({ http, internalAction: internal.uploadthing.handler });
 
 export default http;
