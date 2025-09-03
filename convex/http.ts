3f00a0414e365b967dab3deab852107dc867ce50
islamihab <islam@ryukaian.com>
2025-09-03 18:53:06 +0300

    Add convex-helpers dependency and implement CORS in Convex HTTP router

--- convex/http.ts
+++ convex/http.ts
@@ -1,9 +1,14 @@
 import { httpRouter } from "convex/server";
 import { createRouteHandler } from "uploadthing/convex-helpers";
 import { internal } from "./_generated/api";
+import corsRouter from "convex-helpers/server/cors";
 
 const http = httpRouter();
 
-createRouteHandler({ http, internalAction: internal.uploadthing.handler });
+const cors = corsRouter(http, {
+  allowedOrigins: ["http://localhost:3000"],
+});
+
+createRouteHandler({ http: cors, internalAction: internal.uploadthing.handler });
 
 export default http;
